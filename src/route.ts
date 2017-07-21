9774c3413c3d5c5ec92ed42ff31a2e5dbcbf7ce5
24wings <731502029@qq.com>
2017-07-21 13:54:09 +0800

    修改绑定了 this对象到请求对象上

--- src/route.ts
+++ src/route.ts
@@ -4,13 +4,14 @@ export type Request = express.Request;
 export type Response = express.Response;
 export type RequestHandler = express.RequestHandler;
 
-
 export namespace Route {
     export type Request = express.Request;
     export type Response = express.Response;
     export type RequestHandler = express.RequestHandler;
 
     export interface IRoute {
+        req: Request;
+        res: Response;
 
         doAction: (action: string, method: string, next: RequestHandler) => RequestHandler;
     }
@@ -22,6 +23,11 @@ export namespace Route {
         DELETE = 'delete';
         PUT = 'put';
         public service = service;
+        res: Response;
+        req: Request;
+        doAction(action, method, next) {
+            return next;
+        }
 
         constructor() {
 
@@ -35,17 +41,17 @@ export namespace Route {
          * 
          * 构建路由
          */
-        static buildRoute(routeClass: new () => IRoute): RequestHandler {
-            var route = new routeClass();
+        static buildRoute(routeClass: new () => BaseRoute): RequestHandler {
 
             return (req: Request, res: Response, next: RequestHandler) => {
-
-
+                var route = new routeClass();
+                route.req = req;
+                route.res = res;
                 route.doAction(req.params.action, req.method.toLowerCase(), next).bind(route)(req, res, next);
             }
         }
         static buildMiddle(): RequestHandler[] {
-            return
+            return;
 
         }
 
